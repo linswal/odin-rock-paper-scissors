7247b19a7d450a698d0c2fec7f5d28c9d2445006
linswal <albertlin347@gmail.com>
2025-11-04 11:16:24 -0800

    Add playRound function

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,20 +1,54 @@
-const getComputerChoice = () => {
-    choice = Math.random()
-    if (choice <= 0.3) {
-        return "rock"
-    } else if (choice >= 0.6) {
-        return "paper"
+let humanScore = 0;
+let computerScore;
+
+const playRound = (humanChoice, computerChoice) => {
+    let humanChoiceCapitalized = humanChoice[0].toUpperCase() + humanChoice.slice(1);
+    
+    if (humanChoiceCapitalized === "Rock") {
+        if (computerChoice === "Scissors") {
+            console.log(`You win! ${humanChoiceCapitalized} beats ${computerChoice}`);
+        } else if (computerChoice === "Paper") {
+            console.log(`You lose! ${humanChoiceCapitalized} loses to ${computerChoice}`);
+        } else {
+            console.log(`You draw! ${humanChoiceCapitalized} ties with ${computerChoice}`);
+        };
+    } else if (humanChoiceCapitalized === "Paper") {
+        if (computerChoice === "Rock") {
+            console.log(`You win! ${humanChoiceCapitalized} beats ${computerChoice}`);
+        } else if (computerChoice === "Scissors") {
+            console.log(`You lose! ${humanChoiceCapitalized} loses to ${computerChoice}`);
+        } else {
+            console.log(`You draw! ${humanChoiceCapitalized} ties with ${computerChoice}`);
+        };
     } else {
-        return "scissors"
-    }
-}
+        if (computerChoice === "Paper") {
+            console.log(`You win! ${humanChoiceCapitalized} beats ${computerChoice}`);
+        } else if (computerChoice === "Rock") {
+            console.log(`You lose! ${humanChoiceCapitalized} loses to ${computerChoice}`);
+        } else {
+            console.log(`You draw! ${humanChoiceCapitalized} ties with ${computerChoice}`);
+        };
+    };
+};
+
+const getComputerChoice = () => {
+    choice = Math.random();
+    if (choice <= 0.3) {
+        return "Rock";
+    } else if (choice >= 0.6) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    };
+};
 
 const getHumanChoice = () => {
-    userAnswer = prompt("Pick rock, paper, or scissors: ")
-    return userAnswer
-}
+    userAnswer = prompt("Pick rock, paper, or scissors: ");
+    return userAnswer;
+};
 
-console.log(getHumanChoice())
+playRound(getHumanChoice(), getComputerChoice());
+// console.log(getHumanChoice());
 
 // for (let i = 0; i < 5; i++) {
 //     console.log(getComputerChoice())
